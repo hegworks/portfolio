0d77962b4492a760b2b8822fd040f846ef62cf12
HEGworks <hess.gha@gmail.com>
2021-04-04 17:27:58 +0430

    Added Projects and fixed js for nav active state

--- javascript.js
+++ javascript.js
@@ -1,14 +1,12 @@
-// Get the container element
-var btnContainer = document.getElementsByTagName("ul");
-
 // Get all buttons with class="btn" inside the container
-var btns = document.getElementsByTagName("li");
+var btns = document.querySelectorAll("li a");
+console.log(btns);
+console.log(btns.length);
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
 	btns[i].addEventListener("click", function () {
 		var current = document.getElementsByClassName("active");
-
 		// If there's no active class
 		if (current.length > 0) {
 			current[0].className = current[0].className.replace(" active", "");
